2b3ff363cae50151757123db1c1aab024a09cc96
Luth <noafowl@gmail.com>
2022-06-29 21:24:24 -0600

    Updated the landing if statement

--- Personal Game Project/Assets/Scripts/PlayerController.cs
+++ Personal Game Project/Assets/Scripts/PlayerController.cs
@@ -45,7 +45,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             onGround = true;
         }
