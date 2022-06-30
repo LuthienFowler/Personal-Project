9c8ea5211669fd468b5ac8515027e1603fc5e90c
Luth <noafowl@gmail.com>
2022-06-29 21:13:36 -0600

    Made some simple movement
    You can't turn the camera yet

--- Personal Game Project/Assets/Scripts/PlayerController.cs
+++ Personal Game Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+      /////////////////
+     /// Variables ///
+    /////////////////
+
+    private float speed = 0.2f;
+    private Rigidbody playerRb;
+
+      /////////////////
+     /// Functions ///
+    /////////////////
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Getting the player's rigid body
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Getting the input for the horizontal axis and vertical axis
+        float hi = Input.GetAxis("Horizontal");
+        float vi = Input.GetAxis("Vertical");
+
+        // Simple movement
+        transform.position += Vector3.forward * speed * vi;
+        transform.position += Vector3.right * speed * hi;
         
     }
 
